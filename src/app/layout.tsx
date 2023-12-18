17345ecd8df2133e438af8ca05b588ed7028375e
Sean <96028788+s-corcoran@users.noreply.github.com>
2023-12-17 20:45:52 -0700

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Belgrano } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const belgrano = Belgrano({
   weight: ['400'],
@@ -20,7 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={belgrano.className}>{children}</body>
+      <body className={belgrano.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }

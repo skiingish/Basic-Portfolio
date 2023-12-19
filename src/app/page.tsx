8dd94b3250866fa432932e967b2a93b44daa7992
Sean <96028788+s-corcoran@users.noreply.github.com>
2023-12-18 19:47:08 -0700

    added icons and color background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Github } from 'lucide-react';
+import { Github, Mail, LinkedinIcon } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-around lg:justify-center p-12'>
+    <main className='flex min-h-screen flex-col items-center justify-around lg:justify-center p-12 bg-gradient-to-b from-blue-400/60 ..'>
       <div className='animate-in flex flex-col items-center'>
         <Image
           src='/assets/sean.png'
@@ -26,33 +26,33 @@ export default function Home() {
         <p className='text-lg'>
           Hi, I`m Sean 👋. I build things. Generally with JavaScript,
           TypeScript, React, and Node.js. I also like to play with hardware to
-          connect to the physical world (IoT).
+          connect the physical world (IoT).
         </p>
       </div>
       <div className='h-2 lg:h-8'></div>
       <div className='animate-in flex flex-col items-center rounded-2xl p-2 lg:p-4 max-w-sm lg:max-w-md bg-neutral-700/50'>
         <p className='text-lg'>
           You can find me{' '}
-          <Link
+          <a
             className='text-cyan-200/80 hover:text-cyan-200/100'
             href='https://au.linkedin.com/in/sean-corcoran-aa1732181'
           >
-            here
-          </Link>{' '}
+            here <LinkedinIcon className='inline mt-[-0.19rem]' size={16} />
+          </a>{' '}
           and{' '}
-          <Link
+          <a
             className='text-cyan-200/80 hover:text-cyan-200/100'
             href='https://github.com/skiingish'
           >
-            here
-          </Link>{' '}
-          or send me a email {}{' '}
-          <Link
+            here <Github className='inline mt-[-0.19rem]' size={16} />
+          </a>{' '}
+          or send me an email {}{' '}
+          <a
             className='text-cyan-200/80 hover:text-cyan-200/100'
-            href='mailto:hello@seanbuildsthings.com'
+            href='mailto:hello@seanbuildsthings.com?subject=Hello'
           >
-            here.
-          </Link>
+            here <Mail className='inline mt-[-0.19rem]' size={16} />.
+          </a>
         </p>
       </div>
     </main>
